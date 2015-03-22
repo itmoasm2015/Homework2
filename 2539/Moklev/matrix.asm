8ec48165e0758ee927699f195d7dedd2ae5652e9
Moklev Slava <slavam2605@bk.ru>
2015-03-22 06:19:08 -0400

    new, delete, rows, cols

--- 2539/Moklev/matrix.asm
+++ 2539/Moklev/matrix.asm
@@ -16,11 +16,19 @@
     pop rbp
 %endmacro
 
+%macro fit_4 1    ; makes number divides by 4
+    add %1, 3
+    and %1, ~3
+%endmacro
 
 extern aligned_alloc
 extern malloc
+extern free
 
 global matrixNew
+global matrixDelete
+global matrixGetRows
+global matrixGetCols
 
 ;  # Matrix data type definition
 ;
@@ -35,37 +43,42 @@ section .text
     
 ; Matrix matrixNew(unsigned int rows, unsigned int cols);
 matrixNew: 
-    push_x64 ; save needed registers (from rbx, r12 - r15)
+    push_x64        ; save needed registers (from rbx, r12 - r15)
     mov rbp, rsp
-    push rdi ; save arguments
-    push rsi ; malloc will overwrite them
+    push rdi        ; save arguments
+    push rsi        ; malloc will overwrite them
     
-    mov rdi, 16 ; sizeof(unsigned int) * 2 + sizeof(float*) 
-    call malloc ; void* malloc(size_t size)
+    mov rdi, 16     ; sizeof(unsigned int) * 2 + sizeof(float*) 
+    call malloc     ; void* malloc(size_t size)
     
-    pop rsi ; restore arguments
+    pop rsi         ; restore arguments
     pop rdi 
     
-    test rax, rax  ; if malloc returned NULL
+    test rax, rax   ; if malloc returned NULL
     jz .return
     
 .malloc_ok:
     
-    mov dword [rax], edi
-    mov dword [rax + 4], esi
+    mov dword [rax], edi        ; store Matrix->rows
+    mov dword [rax + 4], esi    ; store Matrix->cols
 
-    push rax ; save return value, aligned_alloc will overwrite it
+    push rax            ; save return value, aligned_alloc will overwrite it
     
+    fit_4 rsi           ; makes sizes divides by 4
+    fit_4 rdi           ; to increase speed of functions
+                        ; because SSE instructions process 
+                        ; 4 numbers by instruction
     mov rax, rdi
     mul rsi
-    mov rsi, rax ; size (rows * cols)
-    push rsi     ; save matrix size for future use
-    mov rdi, 16  ; alignment
-    call aligned_alloc ; void* aligned_alloc(size_t alignment, size_t size)
+    push rax            ; save matrix size for future use
+    shl rax, 2          ; * 4, size of float == 4 bytes
+    mov rsi, rax        ; size (rows * cols * 4 bytes)
+    mov rdi, 16         ; alignment
+    call aligned_alloc  ; void* aligned_alloc(size_t alignment, size_t size)
     mov rcx, rax
 
-    pop rsi ; restore matrix size
-    pop rax ; restore return value
+    pop rsi             ; restore matrix size
+    pop rax             ; restore return value
     mov qword [rax + 8], rcx ; store pointer to matrix data // matrix.data = rcx
 
     ; # Move matrix size to /counter/ register rcx
@@ -76,13 +89,31 @@ matrixNew:
     mov dword [rsi + 4 * rcx - 4], 0.0
     loop .fill_zeroes
    
-
+    ; # just for me
     ; windows: rcx, rdx, r8, r9, stack 
     ; linux:   rdi, rsi, rdx, rcx, r8, r9, stack
     
     
 .return:            
     pop_x64
+    ret  
+   
+matrixDelete:
+    push_x64            ; save registers by x64 convention
+    
+    mov r15, rdi        ; store struct address at safe register
+    mov rdi, [rdi + 8]  ; pass argument of Matrix->(float* data) to free
+    call free           ; free(void* ptr) -- free data
+    mov rdi, r15        ; pass struct address to free
+    call free           ; free(void* ptr) -- free matrix
+
+    pop_x64             ; restore registers
+    ret    
+
+matrixGetRows:
+    mov rax, [rdi]      ; just get Matrix->rows
     ret
-    
-    
+
+matrixGetCols:
+    mov rax, [rdi + 4] ; just get Matrix->cols
+    ret
--- 2539/Moklev/test.c
+++ 2539/Moklev/test.c
@@ -5,16 +5,17 @@
 int main()
 {
     Matrix a = matrixNew(10, 10);
-    //printf("mamka %lld\n", (long long) a);
-    //printf("%d %d\n", *((unsigned int*) a), *((unsigned int*) a + 1));
-    printf("%d %d\n", a->rows, a->cols);
-    printf("%d %d\n", (a->rows + 3) & ~3, (a->cols + 3) & ~3);
-    a->cols = (a->cols + 3) & ~3;
-    a->rows = (a->rows + 3) & ~3;
+    
+    int a_cols = (matrixGetCols(a) + 3) & ~3;
+    int a_rows = (matrixGetRows(a) + 3) & ~3;
+    float* a_data = a->data;
+    
     int i, j;
-    for (j = 0; j < a->rows; j++)
-        for (i = 0; i < a->cols; i++)
-            printf("%.2f%c", a->data[j * a->cols + i], " \n"[i == a->cols - 1]);
+    for (j = 0; j < a_rows; j++)
+        for (i = 0; i < a_cols; i++)
+            printf("%.2f%c", a_data[j * a_cols + i], " \n"[i == a_cols - 1]);
+
+    matrixDelete(a);
     
     return 0;
 }

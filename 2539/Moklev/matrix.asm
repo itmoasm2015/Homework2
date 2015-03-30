b46f234901c44912c0070a408120eab4e99211ed
Moklev Slava <slavam2605@bk.ru>
2015-03-30 18:25:55 +0300

    wut

--- 2539/Moklev/matrix.asm
+++ 2539/Moklev/matrix.asm
@@ -32,6 +32,9 @@ global matrixGetCols
 global matrixGet
 global matrixSet
 global matrixScale
+global matrixAdd
+global matrixMul
+global matrixTranspose
 
 ;  # Matrix data type definition
 ;
@@ -45,11 +48,12 @@ global matrixScale
 section .text
     
 ; Matrix matrixNewExt(unsigned int rows, unsigned int cols, int flag);
-matrixNew:
+; # Allocates new matrix of size rows x cols and initialize with zeroes if flag != 0
+matrixNewExt:
     enter 0, 0 
     push_x64        ; save needed registers (from rbx, r12 - r15)
     mov rbp, rsp
-    ;push rdx            ; save flag
+    push rdx            ; save flag
     push rdi        ; save arguments
     push rsi        ; malloc will overwrite them
     
@@ -86,14 +90,23 @@ matrixNew:
     mov rdi, 16         ; alignment
     call aligned_alloc  ; void* aligned_alloc(size_t alignment, size_t size)
     mov rcx, rax
-
+    
     pop rsi             ; restore matrix size
     pop rax             ; restore return value
-    ;pop rdx             ; restore flag
+    pop rdx             ; restore flag
     mov qword [rax + 8], rcx ; store pointer to matrix data // matrix.data = rcx
+    
+    test rcx, rcx       ; if aligned_alloc returned NULL
+    jnz .after_null
+    mov rdi, rax        ; 1st argument of free -- pointer to matrix
+    call free           ; free alloc'd memory for matrix's struct
+    xor rax, rax        ; return NULL
+    jmp .return     
+        
 
-    ;test edx, edx
-    ;jz .return               ; if flag == 0 than skip initialization
+.after_null:
+    test edx, edx
+    jz .return               ; if flag == 0 than skip initialization
     ; # Move matrix size to /counter/ register rcx
     ; # Fill the matrix data with 0.0f values
     mov ebx, 1
@@ -112,15 +125,17 @@ matrixNew:
     leave
     ret  
 
-;matrixNew:
-;    mov rdx, 1
-;    call matrixNewExt
-;    ret
-;
-;matrixAlloc: 
-;    mov rdx, 0
-;    call matrixNewExt
-;    ret
+; Matrix matrixNew(unsigned int rows, unsigned int cols)
+matrixNew:
+    mov rdx, 1
+    call matrixNewExt
+    ret
+
+; # Allocates matrix of size rows x cols without initialization
+matrixAlloc: 
+    mov rdx, 0
+    call matrixNewExt
+    ret
 
 ; void matrixDelete(Matrix matrix)   
 matrixDelete:
@@ -184,18 +199,19 @@ matrixSet:
 
 ; Matrix matrixScale(Matrix matrix, float k)
 matrixScale:
-    push_x64
     enter 0, 0
+    push_x64
         
     
     mov r15, rdi            ; save pointer to input matrix
-    mov edi, [r15]
-    mov esi, [r15 + 4]
-    ;call matrixAlloc
-    call matrixNew
-    mov rdi, r15
-    mov r14, rax
-    mov r13, [r14 + 8]
+    mov edi, [r15]          ; 1st argument -- rows
+    mov esi, [r15 + 4]      ; 2nd argument -- cols
+    call matrixAlloc        ; allocate matrix of equal size
+    test rax, rax           ; if matrixAlloc returned NULL
+    jz .return              ; return NULL too
+    mov rdi, r15            ; restore pointer to input matrix
+    mov r14, rax            ; save pointer to allocated matrix
+    mov r13, [r14 + 8]      ; save pointer to data of allocated matrix
 
     pshufd xmm0, xmm0, 0    ; copy 1st float from xmm0 to all
         
@@ -211,12 +227,160 @@ matrixScale:
 .multiply_loop:
     movaps xmm1, [rsi + 4 * rax - 16]   ; load 128 bits from aligned memory
     mulps xmm1, xmm0                    ; packed multiply
-    movaps [r13 + 4 * rax - 16], xmm1   ; store 128 bits back
+    movaps [r13 + 4 * rax - 16], xmm1   ; store 128 bits to result
     sub rax, 4                          ; to the next 128 bits!
     jnz .multiply_loop
 
     mov rax, r14 
 
-    leave
+.return:
     pop_x64
+    leave
+    ret
+
+; Matrix matrixAdd(Matrix a, Matrix b)
+matrixAdd:
+    enter 0, 0
+    push_x64
+    
+    xor rax, rax        
+    mov ecx, [rdi]          ; compare rows
+    cmp ecx, [rsi]
+    jne .return             ; return 0 if sizes are not equal
+    mov ecx, [rdi + 4]
+    cmp ecx, [rsi + 4]      ; compare cols
+    jne .return 
+
+    push rdi
+    push rsi
+    mov edi, [rdi]          ; 1st argument -- rows
+    mov esi, ecx            ; 2nd argument -- cols, rest in ecx (high half of rsi unused)
+    call matrixAlloc        ; allocate matrix of equal to input matrices size
+    pop rsi
+    pop rdi
+    
+    test rax, rax           ; if matrixAlloc returned NULL
+    jz .return              ; return NULL too
+    mov r15, rax            ; store pointer to allocated matrix
+    mov r14, [rax + 8]      ; save pointer to allocated matrix's data
+    
+    xor rcx, rcx            ; what are we doing?
+    xor rax, rax            ; yes, cleaning up high 32 bits again
+    mov ecx, [rdi]          ; load sizes again
+    mov eax, [rdi + 4]      ; because call of functions could erase our registers
+    fit_4 rcx               ; makes sizes divides by 4
+    fit_4 rax               ; for SSE packed instructions
+    mul rcx                 ; mul by 64bit register to store entire result in rax
+    mov rdi, [rdi + 8]      ; save pointers to input matrices data
+    mov rsi, [rsi + 8]      ; pointer to input matrices are not needed anymore
+    
+.add_loop:
+    movaps xmm0, [rdi + 4 * rax - 16]   ; load 128 bits from aligned memory for first matrix
+    movaps xmm1, [rsi + 4 * rax - 16]   ; and for second
+    addps xmm1, xmm0                    ; packed add
+    movaps [r14 + 4 * rax - 16], xmm1   ; store 128 bits to result
+    sub rax, 4                          ; to the next 128 bits!
+    jnz .add_loop
+    mov rax, r15            ; return pointer to the resulting matrix    
+
+.return:
+    pop_x64
+    leave 
     ret 
+
+; Matrix matrixTranspose(Matrix a)
+; # Allocates new matrix with size (a->cols x a->rows)
+;   and elements: result[i][j] = a[j][i]
+matrixTranspose:
+    enter 0, 0
+    push_x64
+
+    mov r15, rdi
+    mov edi, [r15 + 4]
+    mov esi, [r15]
+    call matrixAlloc
+    mov r14, rax
+    xor rax, rax
+    xor rsi, rsi
+    mov esi, [r15 + 4]
+    mov eax, [r15]
+    fit_4 rax
+    fit_4 rsi
+    push r11
+    mov r11, rax
+    mul rsi
+    mov r15, [r15 + 8]
+    mov rbx, [r14 + 8]
+    
+    mov rcx, rax
+.fill:
+    mov r13d, dword [r15 + 4 * rcx - 4]
+    mov rax, rcx
+    dec rax
+    xor rdx, rdx
+    div rsi
+    xchg rax, rdx
+    mov r12, rdx
+    mul r11
+    add rax, r12
+    mov dword [rbx + 4 * rax], r13d
+    loop .fill
+    mov rax, r14
+
+    pop r11
+
+.return:
+    pop_x64
+    leave
+    ret
+
+; Matrix matrixMul(Matrix a, Matrix b)
+matrixMul:
+    enter 0, 0
+    push_x64
+        
+    ; # Multiplication is correct only if a->cols == b->cols
+    ;   If it's true, sizes of resulting matrix will be:
+    ;   result->rows = a->rows
+    ;   result->cols = b->rows
+    mov eax, [rdi + 4]      ; load a->cols
+    cmp eax, [rsi + 4]      ; compare with b->cols
+    xor rax, rax            ; return NULL if not equals
+    jne .return 
+
+    push rdi                ; save pointer to a
+    push rsi                ; save pointer to b
+    mov rdi, [rdi]          ; 1st argument is a->rows 
+    mov rsi, [rsi]          ; 2nd argument if b->rows
+    call matrixAlloc        ; allocate resulting matrix
+    pop rsi                 ; restore pointer to b
+    pop rdi                 ; restore pointer to a
+    test rax, rax           ; if matrixAlloc failed
+    jz .return              ; return NULL
+
+    xor r13, r13             ; clear high 32 bits
+    xor r14, r14            
+    xor r15, r15
+    mov r13d, [rdi + 4]      ; save a->cols == b->cols
+    mov r14d, [rdi]          ; save result's rows
+    mov r15d, [rsi]          ; save result's cols
+  
+    push rax
+    push rdi
+    
+    xchg rdi, rsi
+    call matrixTranspose
+    mov rsi, rax
+
+    pop rdi 
+    pop rax
+    
+     
+
+    mov rdi, rsi
+    call free
+         
+.return:
+    pop_x64
+    leave
+    ret
--- 2539/Moklev/test.c
+++ 2539/Moklev/test.c
@@ -1,63 +1,186 @@
+#include <stdio.h>
 #include "matrix.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "time.h"
+#include <math.h>
 
-int main()
-{
-    Matrix a = matrixNew(10, 10);
-    
-    int a_cols = (matrixGetCols(a) + 3) & ~3;
-    int a_rows = (matrixGetRows(a) + 3) & ~3;
-    float* a_data = a->data;
-    
-    int i, j;
-    
-    for (j = 0; j < a_rows; j++)
-        for (i = 0; i < a_cols; i++)
-            matrixSet(a, j, i, (float)(i + j));
+#define EPS 0.001
 
-    Matrix scaled = matrixScale(a, 2.0);
+int matrixEquals(Matrix a, Matrix b) {
+	if(matrixGetRows(a) != matrixGetRows(b))
+		return 0;
+	
+	if(matrixGetCols(a) != matrixGetCols(b))
+		return 0;
+	
+	for(int i = 0; i < matrixGetRows(a); i++)
+		for(int j = 0; j < matrixGetCols(a); j++)
+			if((matrixGet(a, i, j) - matrixGet(b, i, j)) > EPS)
+				return 0;
+	
+	return 1;
+}
 
-    for (j = 0; j < a_rows; j++)
-        for (i = 0; i < a_cols; i++) {
-            //float s = matrixGet(a, j, i);
-            //unsigned int as = *(unsigned int*)&s;
-            printf("%.2f%c", matrixGet(a, j, i), " \n"[i == a_cols - 1]);
-            //printf("%u|%u%c", as, ((unsigned int*)a_data)[j * a_cols + i], " \n"[i == a_cols - 1]);
+float random(int i, int j, int param) {
+	return i * 15 ^ param + (j % (i + 1)) + j * 3 ^ param;
+}
+
+int testCreation(int rows, int cols) {
+	Matrix a = matrixNew(rows, cols);
+	Matrix b = matrixScale(a, 0.0f);
+	
+	int rv = matrixEquals(a, b) * (matrixGetRows(a) == rows) * (matrixGetCols(a) == cols);
+	
+	matrixDelete(a);
+	matrixDelete(b);
+	
+	return rv;
+}
+
+Matrix randomMatrix(int rows, int cols, int param) {
+	Matrix a = matrixNew(rows, cols);
+	for(int i = 0; i < rows; i++)
+		for(int j = 0; j < cols; j++) {
+			matrixSet(a, i, j, random(i, j, param));
+		}
+	return a;
+}
+
+int testScale(int rows, int cols) {
+	Matrix a = randomMatrix(rows, cols, 1);
+	float f = random(rows * cols, rows + cols, 2);
+	Matrix b = matrixScale(a, f);
+	int result = 1;
+	for(int i = 0; i < rows; i++)
+		for(int j = 0; j < cols; j++) {
+			result *= (fabs(matrixGet(b, i, j) - matrixGet(a, i, j) * f) < EPS);
+		}
+	matrixDelete(a);
+	matrixDelete(b);
+	return result;
+}
+
+int testAddition(int rows, int cols) {
+	Matrix a = randomMatrix(rows, cols, 1);
+	Matrix b = randomMatrix(rows, cols, 2);
+	Matrix c = matrixAdd(a, b);
+	int result = 1;
+	for(int i = 0; i < rows; i++)
+		for(int j = 0; j < cols; j++) {
+			result *= (fabs(matrixGet(a, i, j) + matrixGet(b, i, j) - matrixGet(c, i, j)) < EPS);
+		}
+	matrixDelete(a);
+	matrixDelete(b);
+	matrixDelete(c);
+	return result;
+}
+
+int testMul(int rows, int cols) {
+	Matrix a = randomMatrix(rows, cols, 1);
+	Matrix b = randomMatrix(cols, rows, 2);
+	
+	int result = 1;
+	
+	Matrix c = matrixMul(a, a);
+	if(c != 0) {
+		result = (rows == cols);
+		matrixDelete(c); // uh
+	}
+	
+	c = matrixMul(a, b);
+	if(c == 0)
+		result = 0;
+	else {
+		for(int i = 0; i < rows; i++)
+			for(int j = 0; j < rows; j++) {
+				float sum = 0.0f;
+				for(int k = 0; k < cols; k++) {
+					sum += matrixGet(a, i, k) * matrixGet(b, k, j);
+				}
+				result *= ((matrixGet(c, i, j) - sum) < EPS);
+			}
+	}
+	
+	matrixDelete(a);
+	matrixDelete(b);
+	matrixDelete(c);
+	
+	return result;
+}
+
+int testZeroMul(int rows, int cols) {
+	Matrix a = matrixNew(rows, 0);
+	Matrix b = matrixNew(0, cols);
+	
+	int result = 1;
+	
+	Matrix c = matrixMul(a, b);
+	if(c == 0)
+		return 0;
+	
+	Matrix zeros = matrixNew(rows, cols);
+	
+	result *= matrixEquals(c, zeros);
+	
+	matrixDelete(a);
+	matrixDelete(b);
+	matrixDelete(c);
+	matrixDelete(zeros);
+	
+	return result;
+}
+
+#define TEST(a) test++; if(!((a)(rows, cols))) printf("Test failed: %s(%d, %d)\n", #a, rows, cols); else success++;
+
+int main() {
+
+    Matrix a = matrixNew(8, 12);
+    for (int i = 0; i < matrixGetRows(a); i++) {
+        for (int j = 0; j < matrixGetCols(a); j++) {
+            matrixSet(a, i, j, i + j * matrixGetRows(a));
+            printf("%.2f ", matrixGet(a, i, j));
         }
-            //printf("%u%c", ((unsigned int*)a_data)[j * a_cols +  i], " \n"[i == a_cols - 1]);
-
-    matrixDelete(scaled);
-    matrixDelete(a);
-
-    //return 0;
-
-    int k;
-    Matrix b = matrixNew(10000, 10000);
-    //int b_cols = (matrixGetCols(b) + 3) & ~3;
-    //int b_rows = (matrixGetRows(b) + 3) & ~3;
-    //float* b_data = b->data;
-
-    int t = clock();
-    //for (k = 0; k < 10; k++)
-    //for (j = 0; j < b_rows; j++)
-    //    for (i = 0; i < b_cols; i++)
-    //        b_data[j * b_cols + i] *= 2;
-    //printf("time: %d ms\n", (clock() - t) / (CLOCKS_PER_SEC / 1000));    
-
-    matrixDelete(b);
-
-    b = matrixNew(10000, 10000);
-
-    t = clock();
-    for (k = 0; k < 10; k++) {
-        Matrix c = matrixScale(b, 2.0);
-        //matrixDelete(c);
+        printf("\n");
     }
-    printf("time: %d ms\n", (clock() - t) / (CLOCKS_PER_SEC / 1000));    
-    
-    matrixDelete(b);
+
+    a = matrixTranspose(a);
+    for (int i = 0; i < matrixGetRows(a); i++) {
+        for (int j = 0; j < matrixGetCols(a); j++) {
+            printf("%.2f ", matrixGet(a, i, j));
+        }
+        printf("\n");
+    }
 
     return 0;
+	
+	int test = 0;
+	int success = 0;
+	
+	for(int rows = 1; rows <= 16; rows++) {
+		for(int cols = 1; cols <= 16; cols++) {
+			TEST(testCreation);
+			TEST(testScale);
+			TEST(testAddition);
+			TEST(testMul);
+			TEST(testZeroMul);
+		}
+	}
+	
+	printf("Testing don %d/%d\n", success, test);
+	
+	printf("Starting HUGE testing\n");
+	
+	int huge_a[] = {1, 100000, 4, 16, 1024};
+	int huge_b[] = {100000, 1, 1024, 2048, 1024};
+	int huge_c[] = {1, 100000, 4, 16, 1024};
+	
+	for(int i = 0; i < 5; i++) {
+		Matrix a = randomMatrix(huge_a[i], huge_b[i], 1);
+		Matrix b = randomMatrix(huge_b[i], huge_c[i], 2);
+		
+		//matrixDelete(matrixMul(a, b));
+		
+		matrixDelete(a);
+		matrixDelete(b);
+	}
+	
+	return 0;
 }

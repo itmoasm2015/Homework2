cb4c1ead0f6c0596cf83c97276562a81068e4f6b
DimaPhil <dima239philippov@gmail.com>
2015-04-03 16:24:58 +0300

    minor fixes + comments

--- 2539/Filippov/test.cpp
+++ 2539/Filippov/test.cpp
@@ -1,54 +1,91 @@
 #include <bits/stdc++.h>
 #include "matrix.h"
 
-using std::vector;
-
 class VectorMatrix {
-	int n, m;
-	vector<vector<float>> matrix;
+        static const int maxn = 100;
+	unsigned n, m;
+        float matrix[maxn][maxn];
 public:
 	VectorMatrix() {
 		n = m = 0;
-		matrix.clear();
 	}
 
-	void matrixNew(int n, int m) {
+	void _matrixNew(unsigned n, unsigned m) {
 		this->n = n;
 		this->m = m;
-		matrix.resize(n);
-		for (int i = 0; i < n; i++) {
-			matrix[i].resize(m);
-			for (int j = 0; j < m; j++) {
+                for (unsigned i = 0; i != n; ++i) {
+			for (unsigned j = 0; j != m; ++j) {
 				matrix[i][j] = 0.f;
 			}
 		}
 	}
 
-	int matrixGetRows() {
+        unsigned _matrixGetRows() {
 		return n;
 	}
 
-	int matrixGetCols() {
+	unsigned _matrixGetCols() {
 		return m;
 	}
 
-	void matrixSet(int x, int y, float value) {
+	void _matrixSet(unsigned x, unsigned y, float value) {
 		matrix[x][y] = value;
 	}
 
-	float matrixGet(int x, int y) {
+	float _matrixGet(unsigned x, unsigned y) {
 		return matrix[x][y];
 	}
+
+        bool assertEquals(Matrix other) {
+            assert(n == matrixGetRows(other));
+            assert(m == matrixGetCols(other));
+            for (unsigned i = 0; i != n; ++i)
+                for (unsigned j = 0; j != m; ++j)
+                    assert(matrix[i][j] == matrixGet(other, i, j));
+       }
 };
 
 class Tester {
+    const int maxn = 100;
+    int testsCount, testsPassed;
+
+    void initTester() {
+        srand(239017);
+        testsCount = 0;
+        testsPassed = 0;
+    }
+public:
+    Tester() {
+        initTester();
+    }
+
+    void testCreationMatrix() {
+        unsigned n = rand() % maxn + 1;
+        unsigned m = rand() % maxn + 1;
+        Matrix actual = matrixNew(n, m);
+        VectorMatrix expected;
+        expected._matrixNew(n, m);
+        testsCount++;
+        expected.assertEquals(actual);
+        if (actual) {
+           matrixDelete(actual);
+        }
+        testsPassed++;
+    }
+
+    void testAll() {
+        for (int test = 0; test != 100; ++test) {
+            testCreationMatrix();
+        }
+    }    
+
+    void printTestingResult(bool ok = true) {
+        printf("%s, %d/%d passed\n\n", ok ? "OK" : "FAIL", testsPassed, testsCount);  
+    }
 };
 
 int main() {
-	Matrix matrix = matrixNew(4, 5);
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 5; j++)
-                printf("%f%c", matrixGet(matrix, i, j), " \n"[j == 4]); 
-        matrixSet(matrix, 2, 2, 1.0);
-        printf("%lf\n", matrixGet(matrix, 2, 2)); 
+    Tester tester;
+    tester.testAll();
+    tester.printTestingResult();
 }

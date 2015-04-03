89e152cd4381963fc271469bc09690647722f31f
Dmitry Tomp <dmitrytomp@gmail.com>
2015-04-03 20:29:17 +0300

    Multiplication works!

--- 2539/Tomp/matrix.asm
+++ 2539/Tomp/matrix.asm
@@ -1,9 +1,11 @@
 extern aligned_alloc
 extern free
 
-%macro getAligned 1
-        add %1, (ALIGNMENT - 1)
-        and %1, ~(ALIGNMENT - 1)
+%define getAligned(a) alignNumber a, 4
+
+%macro alignNumber 2
+        add %1, (%2 - 1)
+        and %1, ~(%2 - 1)
 %endmacro
 
 %define getRows(r, m) mov r, [m]
@@ -18,8 +20,8 @@ matrixNew:
         mov ebx, eax
         mov edx, esi ; cols
         mov ecx, edx
-        getAligned eax
-        getAligned edx
+        getAligned(eax)
+        getAligned(edx)
 
         mul edx
         shl rdx, 32
@@ -54,7 +56,7 @@ matrixGetCols:
         ; rsi - row
         ; rdx - column
         getCols(ecx, rdi)
-        getAligned ecx
+        getAligned(ecx)
         lea rdi, [rdi + 4 * rdx + 16]
         mov eax, esi
         mul ecx
@@ -90,8 +92,8 @@ matrixScale:
         mov rdi, rax
         mov rax, r12
         mov rcx, r13
-        getAligned eax
-        getAligned ecx
+        getAligned(eax)
+        getAligned(ecx)
         mul ecx
         shl rdx, 32
         mov edx, eax
@@ -133,8 +135,8 @@ matrixAdd:
         call matrixNew
         mov rdi, rax
         mov rax, r14
-        getAligned eax
-        getAligned ebx
+        getAligned(eax)
+        getAligned(ebx)
         mul ebx
         shl rdx, 32
         mov edx, eax
@@ -147,7 +149,7 @@ matrixAdd:
         jz .success
         jmp .loop
 .badSizes:
-        xor eax, eax
+        xor rax, rax
 .success:
         pop r14
         pop r13
@@ -155,4 +157,134 @@ matrixAdd:
         pop rbx
         ret
 
-ALIGNMENT equ 4
+%macro loadAndTranspose 3
+        ; xmm0 - xmm3 - 4x4 matrix chunk
+        movaps xmm0, [%1]
+        movaps xmm1, [%1 + 4 * %2]
+        lea %3, [%1 + 8 * %2]
+        movaps xmm2, [%3]
+        movaps xmm3, [%3 + 4 * %2]
+        movaps xmm6, xmm0
+        movhlps xmm6, xmm1
+        movaps xmm4, xmm2
+        movhlps xmm4, xmm3
+        movaps xmm5, xmm4
+        shufps xmm4, xmm6, ODD_POS
+        shufps xmm5, xmm6, EVEN_POS
+        movaps xmm8, xmm1
+        movlhps xmm8, xmm0
+        movaps xmm6, xmm3
+        movlhps xmm6, xmm2
+        movaps xmm7, xmm6
+        shufps xmm6, xmm8, ODD_POS
+        shufps xmm7, xmm8, EVEN_POS
+        ; xmm4 - xmm7 - transposed chunk
+        ;               of the matrix
+%endmacro
+
+%macro loadAndMultiply 1
+        movaps xmm3, [%1]
+        movaps xmm0, xmm3
+        mulps xmm0, xmm4
+        movaps xmm1, xmm3
+        mulps xmm1, xmm5
+        movaps xmm2, xmm3
+        mulps xmm2, xmm6
+        mulps xmm3, xmm7
+        haddps xmm0, xmm1
+        haddps xmm2, xmm3
+        haddps xmm0, xmm2
+%endmacro
+
+global matrixMul
+matrixMul:
+        push rbx
+        push r12
+        push r13
+        push r14
+        push r15
+
+        mov r12, rdi
+        mov r13, rsi
+        xor rcx, rcx
+        getCols(ecx, r12)
+        mov r15, rcx
+        getRows(edx, r13)
+        cmp ecx, edx
+        jne .badSizes
+        xor rdi, rdi
+        xor rsi, rsi
+        getRows(edi, r12)
+        getCols(esi, r13)
+        mov r14, rdi
+        mov rbx, rsi
+        call matrixNew
+        getAligned(r14)
+        getAligned(rbx)
+        getAligned(r15)
+        add r12, 16
+        add r13, 16
+        ; r12 - 1st matrix
+        ; r13 - 2nd matrix
+        ; rax - result
+        ; r14 - rows
+        ; rbx - cols
+        ; r15 - 1st matrix cols
+        mov r11, r15
+.col1:  mov r8, rbx
+        lea rdi, [rax + 16]
+.col2:  ;loadAndTranspose r13, rbx, rcx
+        movaps xmm0, [r13]
+        movaps xmm1, [r13 + 4 * rbx]
+        lea rcx, [r13 + 8 * rbx]
+        movaps xmm2, [rcx]
+        movaps xmm3, [rcx + 4 * rbx]
+        movaps xmm4, xmm0
+        movlhps xmm4, xmm1
+        movaps xmm6, xmm2
+        movlhps xmm6, xmm3
+        movaps xmm5, xmm4
+        shufps xmm4, xmm6, EVEN_POS
+        shufps xmm5, xmm6, ODD_POS
+        movaps xmm6, xmm1
+        movhlps xmm6, xmm0
+        movaps xmm8, xmm3
+        movhlps xmm8, xmm2
+        movaps xmm7, xmm6
+        shufps xmm6, xmm8, EVEN_POS
+        shufps xmm7, xmm8, ODD_POS
+        mov r9, r14
+        mov r10, r12
+        mov rdx, rdi
+.row1:  loadAndMultiply r10
+        lea r10, [r10 + 4 * r15]
+        addps xmm0, [rdx]
+        movaps [rdx], xmm0
+        lea rdx, [rdx + 4 * rbx]
+        sub r9, 1
+        jnz .row1
+; .row1 end
+        add r13, 16
+        add rdi, 16
+        sub r8, 4
+        jnz .col2
+; .col2 end
+        add r12, 16
+        lea r13, [r13 + 4 * rbx]
+        lea r13, [r13 + 8 * rbx]
+        sub r11, 4
+        jnz .col1
+; .col1 end
+        jmp .success
+.badSizes:
+        xor rax, rax
+.success:
+        pop r15
+        pop r14
+        pop r13
+        pop r12
+        pop rbx
+        ret
+
+ODD_POS equ (3 << 6) + (1 << 4) + (3 << 2) + 1
+EVEN_POS equ (2 << 6) + (0 << 4) + (2 << 2) + 0
--- 2539/Tomp/test.c
+++ 2539/Tomp/test.c
@@ -1,7 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <time.h>
+#include <math.h>
 #include "../../include/matrix.h"
+
+float myMult[4][6];
+const float EPS = 1e-5;
+
+float max(const float a, const float b) {
+    return a < b ? b : a;
+}
 
 int main() {
     Matrix matr = matrixNew(4, 5);
@@ -27,13 +36,48 @@ int main() {
     }
     puts("");
     Matrix added = matrixAdd(matr, scaled);
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 5; j++) {
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 5; j++)
             assert(matrixGet(added, i, j) == matrixGet(matr, i, j) + matrixGet(scaled, i, j));
+    Matrix toMult = matrixNew(5, 6);
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 6; j++) {
+            float value = rand() * 1.f / RAND_MAX;
+            matrixSet(toMult, i, j, value);
+            printf("%f ", value);
+            if (i < 4 && j < 5) 
+                matrixSet(matr, i, j, value);
         }
+        puts("");
     }
+    int cTime = time(0);
+    Matrix multiplied = matrixMul(matr, toMult);
+    printf("%u\n", time(0) - cTime);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 6; j++)
+            printf("%f ", matrixGet(multiplied, i, j));
+        puts("");
+    }
+    puts("");
+    cTime = time(0);
+    for (int k = 0; k < 5; k++)
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 6; j++)
+                myMult[i][j] += matrixGet(matr, i, k) * matrixGet(toMult, k, j);
+    printf("%u\n", time(0) - cTime);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 6; j++)
+            printf("%f ", myMult[i][j]);
+        puts("");
+    }
+    puts("");
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 6; j++)
+            assert(abs(myMult[i][j] - matrixGet(multiplied, i, j)) < max(EPS, abs(myMult[i][j]) * EPS));
     matrixDelete(matr);
     matrixDelete(scaled);
     matrixDelete(added);
+    matrixDelete(toMult);
+    matrixDelete(multiplied);
     return 0;
 }

15a660cf3938a0240f674cb15e95957043f93070
heat_wave <allpicksucks@gmail.com>
2015-04-04 16:43:54 +0300

    Mostly finished, gotta test now.

--- 2537/Malimonov/matrix.asm
+++ 2537/Malimonov/matrix.asm
@@ -15,6 +15,8 @@ global matrixAdd
 global matrixMul
 global matrixCopy
 
+SIZE_OF_FLOAT EQU 4
+
 ;multiples-of-4 alignment macro
 %macro align_to_quad 1
 ;((x + 3) / 4) * 4
@@ -33,12 +35,17 @@ global matrixCopy
     add rax, rsi
 %endmacro
 
+%macro fill_cell 1
+    extractps [r13], xmm0, %1
+    lea r13, [r13 + r10 * 4]
+%endmacro
+
 struc Matrix
     cells           resq 1 ; pointer to float array
-    rows            resq 1 ;
-    columns         resq 1 ;
-    aligned_rows    resq 1 ;
-    aligned_cols    resq 1 ;
+    rows            resq 1 ; original number of rows
+    columns         resq 1 ; original number of cols
+    aligned_rows    resq 1 ; aligned number of rows
+    aligned_cols    resq 1 ; aligned number of cols
 endstruc
 
 ;Matrix matrixNew(unsigned int rows, unsigned int cols)
@@ -66,13 +73,13 @@ matrixNew:
     mov [rax + aligned_cols], rsi
 
     imul rdi, rsi   ; calculate matrix size considering alignment
-    mov rsi, 4      ; sizeof float
+    mov rsi, SIZE_OF_FLOAT
 
     push rcx
     call calloc
     pop rcx
 
-    mov [rcx + cells], rax ; 
+    mov [rcx + cells], rax
 
     mov rax, rcx ; move pointer to Matrix instance to RAX
     
@@ -162,6 +169,8 @@ matrixSet:
 matrixScale:
     punpckldq xmm0, xmm0 ; ?:?:k:k
     punpckldq xmm0, xmm0 ; k:k:k:k
+    ; a trick I learned from fellow students
+    ; to easily get 4 instances of the coefficient in XMM0
 
     call matrixCopy
 
@@ -173,7 +182,7 @@ matrixScale:
     movups xmm1, [r8]   ; load a vector of cells to XMM1
     mulps xmm1, xmm0    ; multiply by XMM0
     movups [r8], xmm1   ; return the values to the corresponding cells
-    add r8, 16          ; move the cell pointer by 4 * sizeof float
+    add r8, 4 * SIZE_OF_FLOAT ; move the cell pointer
     sub rcx, 4          ; 4 cells processed, subtract until we've scaled the whole matrix
     jnz .mul_loop
 
@@ -233,5 +242,151 @@ matrixAdd:
 .return:
     ret
 
-;Matrix matrixMul(Matrix a, Matrix b)
+;Matrix matrixTranspose(Matrix matrix)
+;auxiliary operation: create a transposed matrix based on an existing one
+;input:     RDI: pointer to the original matrix
+;return:    RAX: pointer to the transposed instance
+matrixTranspose:
+    push r12        ; save registers state
+    push r13        
+    mov r8, rdi
 
+    mov rdi, [r8 + cols]
+    mov rsi, [r8 + rows]
+    push r8
+    call matrixNew
+    pop r8
+    mov rdi, r8
+
+    mov r8, [rdi + cells] ; original matrix cells
+    mov r9, [rax + cells] ; new matrix cells
+    mov r10, [rdi + aligned_rows]
+    mov r11, [rdi + aligned_cols]
+
+    xor rcx, rcx ; null out the loop counter
+
+.outer:
+    xor r12, r12 ; processed cells counter
+    lea r13, [r9 + rcx * 4] ; address of the first output cell
+
+.inner:
+    movups xmm0, [r8] ; XMM0 = A:B:C:D
+    fill_cell 0
+    fill_cell 1
+    fill_cell 2
+    fill_cell 3
+    
+    add r8, 4 * SIZE_OF_FLOAT ; move the cell pointer
+    add r12, 4 ; update the processed cells counter
+    cmp r12, r11 ; R12 == R11 means that one line of original size
+                 ; has been transposed, so we should process next one
+    jl .inner
+    inc rcx
+    cmp rcx, r10
+    jl .outer
+
+    pop r13
+    pop r12     ; restore registers state
+    ret
+
+;Matrix matrixMul(Matrix a, Matrix b)
+;creates a new matrix c = a * b
+;input:     RDI: pointer to the first matrix
+;           RSI: pointer to the second matrix
+;return:    RAX: pointer to resulting matrix
+matrixMul:
+    push r14    ; save registers state
+    push r15
+    push rbp
+
+    ; dimension compatibility check
+    ; we need the dimensions of the matrices to be
+    ; x * y and y * z, respectively
+    mov r8, [rdi + cols]
+    mov r9, [rsi + rows]
+    cmp r8, r9
+    jne .incompatible
+
+    mov r8, rdi
+    mov r9, rsi
+
+    xchg rdi, rsi ; swap RSI, RDI to transpose the second matrix
+
+    push r8
+    push r9
+
+    call matrixTranspose ; transposed second matrix is in RAX
+
+    mov rbp, rax ; transposed matrix is in RBP
+    pop r9
+    pop r8
+
+    mov rdi, [r8 + rows] ; get the dimensions of the resulting matrix
+    mov rsi, [r9 + cols] ; to call matrixNew
+
+    push r8
+    push r9
+    call matrixNew  ; RAX points to the resulting matrix 
+                    ; of dimensions x * z
+
+    pop r9
+    pop r8
+    mov rdi, r8
+    mov rsi, r9
+    mov r8, [rdi + cells] ; cells of the first matrix
+    mov r9, [rbp + cells] ; cells of the second matrix transposed
+    mov r14, [rax + cells] ; cells of the resulting matrix
+    mov r10, [rsi + aligned_cols] ;?
+    mov r11, [rdi + aligned_rows] ;?
+
+    mov rdx, [rdi + aligned_cols]
+    shl rdx, 2 ;
+
+    mov rsi, r9
+    mov rdi, r10
+
+.l1:
+    mov r10, rdi
+    mov r9, rsi
+
+.l2:
+    xor r15, r15 ; 
+    xorps xmm0, xmm0 ;
+
+.l3:
+    movups xmm1, [r8 + r15] ; XMM1 = A:B:C:D
+    movups xmm2, [r9 + r15] ; XMM2 = E:F:G:H
+    mulps xmm1, xmm2        ; XMM1 = A*E:B*F:C*G:D*H
+    haddps xmm1, xmm1       ; XMM1 = A*E+B*F:C*G+D*H:A*E+B*F:C*G+D*H
+    haddps xmm1, xmm1       ; XMM1 = A*E+B*F+C*G+D*H:~:~:~
+    addss xmm0, xmm1        ; add XMM1 to XMM0
+    add r15, 4 * SIZE_OF_FLOAT ; move pointer
+    cmp r15, rdx            ; check if the line has been processed
+    jl .l3
+
+    add r9, rdx
+    movss [r14], xmm0 ; move result to new matrix cell
+    add r14, 4
+    dec r10
+    jnz .l2 ; r10 == 0 means we've processed a line
+            ; and we can proceed to the next line of the first matrix
+
+    add r8, rdx
+    dec r11
+    jnz .l1 ; r11 == 0 means we've processed everything and can finish
+
+    push rax
+    mov rdi, rbp
+    call matrixDelete ; we no longer need the transposed second matrix
+
+    pop rax
+    jmp .return
+
+.incompatible:
+    mov rax, 0  ; matrices not suitable for multiplication => return 0
+
+.return:
+    pop rbp
+    pop r15
+    pop r14     ; restore registers state
+    ret

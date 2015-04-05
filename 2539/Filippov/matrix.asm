635ed483989c6138a0ecbbd8b075874a8dd9a710
DimaPhil <dima239philippov@gmail.com>
2015-04-05 21:36:09 +0300

    fixed

--- 2539/Filippov/matrix.asm
+++ 2539/Filippov/matrix.asm
@@ -302,12 +302,12 @@ matrixMul:
     pop r12                         ; restoring R12
 
     ; now let's delete matrix, stored in RSI, we don't need it anymore
-    push rdi                        ; RDI is first matrix, we need to save it
-    push rax                        ; RAX is pointer to new matrix, we need to save it
-    mov rdi, rsi                    ; RDI = RSI -- preparing to call free function
-    call free                       ; removing pointer to RSI
-    pop rax                         ; restoring RAX
-    pop rdi                         ; restoring RDI
+    ;push rdi                        ; RDI is first matrix, we need to save it
+    ;push rax                        ; RAX is pointer to new matrix, we need to save it
+    ;mov rdi, rsi                    ; RDI = RSI -- preparing to call free function
+    ;call free                       ; removing pointer to RSI
+    ;pop rax                         ; restoring RAX
+    ;pop rdi                         ; restoring RDI
 
     mov rsi, rax                    ; moving new matrix to RSI, now RSI is transposed matrix
 .multiplying:

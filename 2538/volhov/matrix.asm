7a4af702153fc51d2b35444678751780fc9e4dc1
volhovM <volhovm.cs@gmail.com>
2015-04-11 00:36:29 +0300

    addition added

--- 2538/volhov/matrix.asm
+++ 2538/volhov/matrix.asm
@@ -1,8 +1,10 @@
-;;; Matrix is {rows, columns, ⌈rows⌉, ⌈columns⌉, data}, where:
+;;; Matrix is {rows, columns, ⌈rows⌉, ⌈columns⌉, 16b_offset, data}, where:
 ;;; rows, columns:     4 + 4 bytes (two ints)
 ;;; ⌈rows⌉, ⌈columns⌉: 4 + 4 bytes (two ints)
 ;;; data:              ⌈rows⌉×⌈columns⌉×4 bytes (4 for float).
-;;; (⌈a⌉ = min{x | x≥a && x%4==0}
+;;; (⌈a⌉ = min{x | x≥a && x%8==0}
+
+;;; I use sse2/avx here
 global matrixNew
 global matrixDelete
 global matrixGetRows
@@ -16,28 +18,37 @@ global matrixMul
 extern aligned_alloc
 extern free
 
+%define complement_offset 8     ;8 floats fit into ymm* register
+%define metadata_offset 32      ;4 ints + 16b of nothing (for aligning)
+
 ;;; Matrix matrixNew(unsigned int rows, unsigned int cols);
 matrixNew:
         mov     r8, rdi
         mov     r9, rsi
 
-        ;; complement rdi to 4 if needed
+        ;; complement rdi if needed
         mov     rax, rdi
-        mov     rcx, 4
+        mov     rcx, complement_offset
         xor     rdx, rdx
         div     rcx
-        mov     rbx, 4
+        cmp     rdx, 0
+        je      .skip1
+        mov     rbx, complement_offset
         sub     rbx, rdx
         add     rdi, rbx
+        .skip1
 
-        ;; complement rsi to 4 if needed
+        ;; complement rsi if needed
         mov     rax, rsi
-        mov     rcx, 4
+        mov     rcx, complement_offset
         xor     rdx, rdx
         div     rcx
-        mov     rbx, 4
+        cmp     rdx, 0
+        je      .skip2
+        mov     rbx, complement_offset
         sub     rbx, rdx
         add     rsi, rbx
+        .skip2
 
         ;; store for metadata
         push    rsi
@@ -48,7 +59,7 @@ matrixNew:
         mul     rsi             ; get size of matrix
         mov     r10, 4
         mul     r10             ; each element is 4B float
-        add     rax, 4          ; for 4 numbers of metadata
+        add     rax, metadata_offset ; for 4 numbers of metadata
         mov     rsi, rax
         push    rdi
         push    rsi
@@ -106,28 +117,160 @@ matrixGetCols:
 matrixGet:
         mov     r8d, dword [rdi]
         mov     r9d, dword [rdi+4]
-        cmp     rsi, r8d
-        jg      .failure
-        cmp     rdx, r9d
-        jg      .failure
+        cmp     esi, r8d
+        jge     .failure
+        cmp     edx, r9d
+        jge     .failure
 
-        fld1                    ;return 1 for lulz
+
+        imul    rsi, 4          ; 4 bytes for each float
+        imul    rsi, [rdi+12]   ; × ⌈rows⌉
+
+        imul    rdx, 4          ; 4 bytes for each row element
+        add     rdi, rsi        ; sum it all
+        add     rdi, rdx
+        add     rdi, metadata_offset ; skip metadata
+        movss   xmm0, [rdi]
+
+        jmp     .return
+        .failure
+        mov     rax, [0]
+        .return
+        ret
+
+
+;;; void matrixSet(Matrix matrix, unsigned int row, unsigned int col, float value)
+matrixSet:
+        mov     r8d, dword [rdi]
+        mov     r9d, dword [rdi+4]
+        cmp     esi, r8d
+        jge     .failure
+        cmp     edx, r9d
+        jge     .failure
+
+
+        imul    rsi, 4          ; 4 bytes for each float
+        imul    rsi, [rdi+12]   ; × ⌈columns⌉
+
+        imul    rdx, 4          ; 4 bytes for each row element
+        add     rdi, rsi        ; sum it all
+        add     rdi, rdx
+        add     rdi, metadata_offset ; skip metadata
+        movss   dword [rdi], xmm0
 
         jmp     .return
         .failure
         mov     rax, [0]
         .return
 
-
-
-;;; void matrixSet(Matrix matrix, unsigned int row, unsigned int col, float value)
-matrixSet:
+        ret
 
 ;;; Matrix matrixScale(Matrix matrix, float k)
 matrixScale:
+        mov     rsi, rdi
+        xor     r8, r8
+        xor     r9, r9
+        mov     r8d, dword[rdi]    ; rows
+        mov     r9d, dword[rdi+12] ; ⌈columns⌉
+        add     rdi, metadata_offset
+
+        ;; move rows×⌈columns⌉×4 into rax
+        mov     rax, r8         ; r8
+        mul     r9              ; × r9
+        mov     r10, 4
+        mul     r10             ; x 4b for float
+
+        ;; add end of matrix to rsi (that is rdi for now)
+        add     rsi, metadata_offset
+        add     rsi, rax
+
+        ;; move k to 8 cells in ymm1
+        sub     rsp, 4
+        vmovss  dword[rsp], xmm0
+        vbroadcastss ymm1, dword[rsp]
+        add     rsp, 4
+
+        ;; for each pack of 8 cells (8 * 32bit = 256bit)
+        ;; scale it with ymm1 containing k
+        .loop
+        vmulps  ymm0, ymm1, [rdi]
+        vmovaps [rdi], ymm0
+        add     rdi, 32         ; 8b * 4b for float
+        cmp     rdi, rsi
+        jl      .loop
+        ret
 
 ;;; Matrix matrixAdd(Matrix a, Matrix b);
 matrixAdd:
+        push    r12
+        push    r13
+        push    r14
+        push    r15
+
+        mov     r10, rdi        ; r10 - matrix a
+        mov     r11, rsi        ; r11 - matrix b
+
+        ;; check if a.rows == b.rows
+        mov     r8d, dword[r10]
+        mov     r9d, dword[r11]
+        cmp     r8d, r9d
+        jne     .fail
+
+        ;; check if a.cols == b.cols
+        mov     r8d, dword[r10+4]
+        mov     r9d, dword[r11+4]
+        cmp     r8d, r9d
+        jne     .fail
+
+        ;; save a, b
+        xor     rdi, rdi
+        xor     rsi, rsi
+        mov     edi, dword[r10]
+        mov     esi, dword[r10+4]
+
+        ;; create new matrix of proper size
+        push    r10
+        push    r11
+        call    matrixNew
+        pop     r11
+        pop     r10
+
+        ;; save new matrix into r12
+        mov     r12, rax
+
+        ;; calculate number of elements in matrix
+        xor     rax, rax
+        mov     eax, dword[r12]   ; move rows
+        mul     dword[r12+12]     ; *⌈cols⌉
+        mov     ecx, eax
+        mov     rax, r12
+
+        add     r10, metadata_offset
+        add     r11, metadata_offset
+        add     r12, metadata_offset
+
+        .loop
+        vmovups ymm1, [r10]
+        vmovups ymm2, [r11]
+        vaddps  ymm0, ymm1, ymm2
+        vmovups [r12], ymm0
+        add     r10, 32        ; 8 * 4b for vector of floats
+        add     r11, 32        ; -//-
+        add     r12, 32        ; -//-
+        sub     ecx, complement_offset
+        cmp     ecx, 0
+        jne     .loop
+
+        jmp     .return
+        .fail
+        xor     rax, rax
+        .return
+        pop     r15
+        pop     r14
+        pop     r13
+        pop     r12
+        ret
 
 ;;; Matrix matrixMul(Matrix a, Matrix b);
 matrixMul:
+        ret
--- 2538/volhov/test.cpp
+++ 2538/volhov/test.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
 #include <matrix.h>
+#include <algorithm>
 
 using namespace std;
 
+void dumpMatrix(Matrix m) {
+    if (m == NULL) {
+        printf("NULL MATRIX GIVEN");
+        return;
+    }
+    int cols = matrixGetCols(m);
+    int rows = matrixGetRows(m);
+    cout << endl;
+    for (int i=0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%10g", matrixGet(m, i, j));
+        }
+        cout << endl;
+    }
+}
+
 int main() {
-    std::cout << "test";
-    Matrix m = matrixNew(9, 5);
-    cout << m << endl;
-    cout << matrixGetRows(m) << endl;
-    cout << matrixGetCols(m) << endl;
-    //    matrixMul(matrixNew(2,3), matrixNew(2,3));
+    Matrix m = matrixNew(17, 9);
+    float init = 1.2;
+    for (int i = 0; i < matrixGetRows(m); i++) {
+        for (int j = 0; j < matrixGetCols(m); j++) {
+            matrixSet(m, i, j, init);
+            init += 1.4;
+        }
+    }
+    for (int i = 0; i < min(matrixGetRows(m), matrixGetCols(m)); i++) {
+        matrixSet(m, i, i, 0);
+    }
+    dumpMatrix(m);
+    matrixScale(m, 2.0);
+    dumpMatrix(m);
+    dumpMatrix(m);
+    Matrix n = matrixNew(1,1);
+    Matrix sum = matrixAdd(m, m);
+    dumpMatrix(sum);
+
     matrixDelete(m);
     //cout << matrixGetRows(m) << endl;
     //cout << matrixGetCols(m) << endl;
